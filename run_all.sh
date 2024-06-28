5025a17a2c5b95a2632c0690c9bdefaf66d9bab9
faira <liyifei22@mails.tsinghua.edu.cn>
2024-06-28 16:01:05 +0800

    动一下样例

--- run_all.sh
+++ run_all.sh
@@ -8,7 +8,7 @@ cmake --build build
 mkdir -p output
 # build/PA1 testcases/scene01_basic.txt output/scene01.bmp 50
 # build/PA1 testcases/scene02_cube.txt output/scene02.bmp
-# build/PA1 testcases/scene03_sphere.txt output/scene03.bmp
+build/PA1 testcases/scene03_sphere.txt output/scene031.bmp 50
 # build/PA1 testcases/scene04_axes.txt output/scene04.bmp
 # build/PA1 testcases/scene05_bunny_200.txt output/scene05.bmp
 # build/PA1 testcases/scene06_bunny_1k.txt output/scene06.bmp
